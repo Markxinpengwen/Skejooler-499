8146c2aa6989ef7cdbb91199ff53a0e8f7e0ac1f
Brett <brett_schaad@hotmail.com>
2017-01-24 21:44:06 -0800

    Added edit button to each schedule entry.

--- resources/views/center/schedule.blade.php
+++ resources/views/center/schedule.blade.php
@@ -5,46 +5,55 @@
 @section('content')
     <table>
 
-        <tr><th colspan = "5">Upcoming Exams</th></tr>
+        <tr><th colspan = "6">Upcoming Exams</th></tr>
 
         {{--TODO - need schedule table then implement display--}}
-        @php $i = 0 @endphp
-        @while($i < 3)
+        @php $id = 0 @endphp
+        @while($id < 3)
             <tr>
+                {{ Form::open(array('action' => 'CenterController@showSchedule')) }}
+                {{ Form::hidden('id', $id) }}
                 <td>April 10th, 2016</td>
                 <td>11:00am</td>
                 <td>POL SCI 100</td>
                 <td>BCIT - Vancouver</td>
                 <td>Midterm 1</td>
-                @php $i++ @endphp
+                <td>{{ Form::submit('View Details') }}</td>
+                @php $id++ @endphp
             </tr>
         @endwhile
 
-        <tr><th colspan = "5"><hr>Exams Pending Approval</th></tr>
+        <tr><th colspan = "6"><hr>Exams Pending Approval</th></tr>
 
-        @php $i = 0 @endphp
-        @while($i < 6)
+        @php $id = 0 @endphp
+        @while($id < 6)
             <tr>
+                {{ Form::open(array('action' => 'CenterController@showSchedule')) }}
+                {{ Form::hidden('id', $id) }}
                 <td>April 10th, 2016</td>
                 <td>11:00am</td>
                 <td>POL SCI 100</td>
                 <td>BCIT - Vancouver</td>
                 <td>Midterm 1</td>
-                @php $i++ @endphp
+                <td>{{ Form::submit('View Details') }}</td>
+                @php $id++ @endphp
             </tr>
         @endwhile
 
-        <tr><th colspan = "5"><hr>Past Exams</th></tr>
+        <tr><th colspan = "6"><hr>Past Exams</th></tr>
 
-        @php $i = 0 @endphp
-        @while($i < 5)
+        @php $id = 0 @endphp
+        @while($id < 5)
             <tr>
-                <td>{{ $i }}</td>
-                <td>{{ $i }}</td>
-                <td>{{ $i }}</td>
-                <td>{{ $i }}</td>
-                <td>{{ $i }}</td>
-                @php $i++ @endphp
+                {{ Form::open(array('action' => 'CenterController@showSchedule')) }}
+                {{ Form::hidden('id', $id) }}
+                <td>{{ $id }}</td>
+                <td>{{ $id }}</td>
+                <td>{{ $id }}</td>
+                <td>{{ $id }}</td>
+                <td>{{ $id }}</td>
+                <td>{{ Form::submit('View Details') }}</td>
+                @php $id++ @endphp
             </tr>
         @endwhile
 

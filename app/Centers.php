041cead5175f99bfa5f8435605209df4b359b7d3
Brett <brett_schaad@hotmail.com>
2017-02-02 13:30:56 -0800

    hotfix from previous commit

--- app/Centers.php
+++ app/Centers.php
@@ -1,16 +1,38 @@
 <?php
 
 namespace App;
+use app\Http\Requests\YourRequest as Request;
 
 class Centers extends BaseModel
 {
+
     protected $table = "centers";
     protected $primaryKey = "cid";
+    public $timestamps = true;
 
     protected $rules = array(
-        'cid' => 'required',
-        'name' => 'required',
+        'cid' => 'required|unique',
+        'name' => 'required|string',
+        'email' => 'email',
+        'phone' => 'digits_between:10, 10',
+        'description' => '',
+        'canSupportOnlineExam' => 'boolean',
+        'cost' => 'numeric',
+        'street_name' => 'string',
+        'city' => 'string',
+        'province' => 'string',
+        'country' => 'string',
+        'postal_code' => 'between:7,7',
+        'longitude' => '',
+        'latitude' => ''
     );
+
+    public function messages()
+    {
+        return [
+            '' => '',
+        ];
+    }
 
     public function centers()
     {

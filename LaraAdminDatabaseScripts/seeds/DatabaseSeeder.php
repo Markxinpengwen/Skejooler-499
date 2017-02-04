933e148e5c920dd23e3b1ec5a2d482b5446e45b0
snyperhawk25 <barrettsharpe25@gmail.com>
2017-02-03 18:18:08 -0800

    Fixed issues with above two seeders. LA compliant, tested, and functional.
    **Note: Seeder now requires (y/n) input on decisions such as reseeding standard LA code, and using a constant for the seed of the random-number generator (for identical data set synthesis).**

--- LaraAdminDatabaseScripts/seeds/DatabaseSeeder.php
+++ LaraAdminDatabaseScripts/seeds/DatabaseSeeder.php
@@ -8,9 +8,12 @@ use Dwij\Laraadmin\Models\ModuleFieldTypes;
 use Dwij\Laraadmin\Models\Menu;
 use Dwij\Laraadmin\Models\LAConfigs;
 
+//Used by LA
 use App\Role;
 use App\Permission;
 use App\Models\Department;
+
+include 'database\seeds\InstitutionsTableSeederLA.php';
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,144 +24,158 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		
-		echo "DatabaseSeeder] Running Standard LaraAdmin Seeder Code...\n";
-		
-		/* ================ LaraAdmin Seeder Code ================ */
-		
-		// Generating Module Menus
-		$modules = Module::all();
-		$teamMenu = Menu::create([
-			"name" => "Team",
-			"url" => "#",
-			"icon" => "fa-group",
-			"type" => 'custom',
-			"parent" => 0,
-			"hierarchy" => 1
-		]);
-		foreach ($modules as $module) {
-			$parent = 0;
-			if($module->name != "Backups") {
-				if(in_array($module->name, ["Users", "Departments", "Employees", "Roles", "Permissions"])) {
-					$parent = $teamMenu->id;
-				}
-				Menu::create([
-					"name" => $module->name,
-					"url" => $module->name_db,
-					"icon" => $module->fa_icon,
-					"type" => 'module',
-					"parent" => $parent
-				]);
-			}
-		}
+	    //PHP Laravel Artisan Seeder Workaround
+        //Skip: Bypass Laraadmin Standard Seeder Code.
+        $skip=false;
+        echo "\nSkip Standard LA Seeder Code? (y/n):";
+        $fp = fopen("php://stdin","r");
+        $input = rtrim(fgets($fp, 1024));
+		if($input=="y" || $input=="Y"){
+		    $skip=true;
+		    echo "Skipping LA Seeder Code.\n";
+        }
 
-		// Create Administration Department
-	   	$dept = new Department;
-		$dept->name = "Administration";
-		$dept->tags = "[]";
-		$dept->color = "#000";
-		$dept->save();
-		
-		// Create Super Admin Role
-		$role = new Role;
-		$role->name = "SUPER_ADMIN";
-		$role->display_name = "Super Admin";
-		$role->description = "Full Access Role";
-		$role->parent = 1;
-		$role->dept = $dept->id;
-		$role->save();
-		
-		// Set Full Access For Super Admin Role
-		foreach ($modules as $module) {
-			Module::setDefaultRoleAccess($module->id, $role->id, "full");
-		}
-		
-		// Create Admin Panel Permission
-		$perm = new Permission;
-		$perm->name = "ADMIN_PANEL";
-		$perm->display_name = "Admin Panel";
-		$perm->description = "Admin Panel Permission";
-		$perm->save();
-		
-		$role->attachPermission($perm);
-		
-		// Generate LaraAdmin Default Configurations
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sitename";
-		$laconfig->value = "LaraAdmin 1.0";
-		$laconfig->save();
+		//Skip
+        if(!$skip) {
+            echo "DatabaseSeeder] Running Standard LaraAdmin Seeder Code...\n";
 
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sitename_part1";
-		$laconfig->value = "Lara";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sitename_part2";
-		$laconfig->value = "Admin 1.0";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sitename_short";
-		$laconfig->value = "LA";
-		$laconfig->save();
+            /* ================ LaraAdmin Seeder Code ================ */
 
-		$laconfig = new LAConfigs;
-		$laconfig->key = "site_description";
-		$laconfig->value = "LaraAdmin is a open-source Laravel Admin Panel for quick-start Admin based applications and boilerplate for CRM or CMS systems.";
-		$laconfig->save();
+            // Generating Module Menus
+            $modules = Module::all();
+            $teamMenu = Menu::create([
+                "name" => "Team",
+                "url" => "#",
+                "icon" => "fa-group",
+                "type" => 'custom',
+                "parent" => 0,
+                "hierarchy" => 1
+            ]);
+            foreach ($modules as $module) {
+                $parent = 0;
+                if ($module->name != "Backups") {
+                    if (in_array($module->name, ["Users", "Departments", "Employees", "Roles", "Permissions"])) {
+                        $parent = $teamMenu->id;
+                    }
+                    Menu::create([
+                        "name" => $module->name,
+                        "url" => $module->name_db,
+                        "icon" => $module->fa_icon,
+                        "type" => 'module',
+                        "parent" => $parent
+                    ]);
+                }
+            }
 
-		// Display Configurations
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "sidebar_search";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "show_messages";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "show_notifications";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "show_tasks";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "show_rightsidebar";
-		$laconfig->value = "1";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "skin";
-		$laconfig->value = "skin-white";
-		$laconfig->save();
-		
-		$laconfig = new LAConfigs;
-		$laconfig->key = "layout";
-		$laconfig->value = "fixed";
-		$laconfig->save();
+            // Create Administration Department
+            $dept = new Department;
+            $dept->name = "Administration";
+            $dept->tags = "[]";
+            $dept->color = "#000";
+            $dept->save();
 
-		// Admin Configurations
+            // Create Super Admin Role
+            $role = new Role;
+            $role->name = "SUPER_ADMIN";
+            $role->display_name = "Super Admin";
+            $role->description = "Full Access Role";
+            $role->parent = 1;
+            $role->dept = $dept->id;
+            $role->save();
 
-		$laconfig = new LAConfigs;
-		$laconfig->key = "default_email";
-		$laconfig->value = "test@example.com";
-		$laconfig->save();
-		
-		$modules = Module::all();
-		foreach ($modules as $module) {
-			$module->is_gen=true;
-			$module->save();	
-		}
-		
+            // Set Full Access For Super Admin Role
+            foreach ($modules as $module) {
+                Module::setDefaultRoleAccess($module->id, $role->id, "full");
+            }
+
+            // Create Admin Panel Permission
+            $perm = new Permission;
+            $perm->name = "ADMIN_PANEL";
+            $perm->display_name = "Admin Panel";
+            $perm->description = "Admin Panel Permission";
+            $perm->save();
+
+            $role->attachPermission($perm);
+
+            // Generate LaraAdmin Default Configurations
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "sitename";
+            $laconfig->value = "LaraAdmin 1.0";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "sitename_part1";
+            $laconfig->value = "Lara";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "sitename_part2";
+            $laconfig->value = "Admin 1.0";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "sitename_short";
+            $laconfig->value = "LA";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "site_description";
+            $laconfig->value = "LaraAdmin is a open-source Laravel Admin Panel for quick-start Admin based applications and boilerplate for CRM or CMS systems.";
+            $laconfig->save();
+
+            // Display Configurations
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "sidebar_search";
+            $laconfig->value = "1";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "show_messages";
+            $laconfig->value = "1";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "show_notifications";
+            $laconfig->value = "1";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "show_tasks";
+            $laconfig->value = "1";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "show_rightsidebar";
+            $laconfig->value = "1";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "skin";
+            $laconfig->value = "skin-white";
+            $laconfig->save();
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "layout";
+            $laconfig->value = "fixed";
+            $laconfig->save();
+
+            // Admin Configurations
+
+            $laconfig = new LAConfigs;
+            $laconfig->key = "default_email";
+            $laconfig->value = "test@example.com";
+            $laconfig->save();
+
+            $modules = Module::all();
+            foreach ($modules as $module) {
+                $module->is_gen = true;
+                $module->save();
+            }
+
+        }//Skip
+
 		echo "DatabaseSeeder] \t- Completed\n";
 		
 		/* ======*=====*===== Custom Seeder Code ====*====*====*==== */

b056647e2c74ba238a004e22391c141d5cee2db8
snyperhawk25 <barrettsharpe25@gmail.com>
2017-03-09 14:57:23 -0800

    Updated default values for attributes in the migrations.
    UI Overhaul to student exam booking blade.

--- resources/views/student/examRequestForm.blade.php
+++ resources/views/student/examRequestForm.blade.php
@@ -10,13 +10,53 @@
 
         <!-- Styles -->
         <style>
+            /*Skejooler Colour Variable #52ffe5*/
+            :root{
+
+            }
+
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: #000000; /*was 636B6F*/
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
+            }
+
+            /*Form Fields*/
+
+            /* from: http://jsfiddle.net/bQ859/  */
+            .required:after {
+                content:" ***";
+                color: red;
+                /*font-size: x-large;*/
+
+            }
+
+            .tableForm{
+                /*NOT WORKING*/
+                --skejooler-color: #52ffe5;
+            }
+
+            .tableForm tr{
+                /*color: #CC00FF; /*test remove*/
+                text-align: left;
+                padding: 20px 5px 20px 10px;
+            }
+
+            .tableForm input[type=text] {
+                border: 2px solid lightgray;
+                border-radius: 2px;
+            }
+
+            .tableForm input[type=text]:focus,input[type=email]:focus, input[type=number]:focus, input[type=date]:focus {
+                border: 2px solid;
+                border-color: #207dff;
+            }
+
+            .response{
+
             }
 
             .full-height {
@@ -96,7 +136,8 @@
             var onlineColumn = "OnlineSupport";
             var addressColumn = "Address";
             var locationColumn = "Latitude";
-            //City Table //Any City = Burnaby Default
+
+            //Old City Table //Any City = Burnaby Default
             var City = [
                 'Any City',
                 '100 Mile House',
@@ -105,20 +146,69 @@
                 'Kelowna'
             ];
 
-            //City Table (sorted by pop.)
-            var CityByPop = ['Vancouver', 'Surrey', 'Burnaby', 'Richmond', 'Abbotsford', 'Kelowna', 'Port Coquitlam', 'Delta', 'Kamloops', 'Nanaimo', 'Victoria', 'Chilliwack', 'Maple Ridge', 'Prince George', 'New Westminster', 'North Vancouver', 'Penticton', 'West Vancouver', 'North Cowichan', 'Courtenay', 'Vernon', 'Central Okanagan', 'Campbell River', 'Port Moody', 'Walnut Grove', 'Langley', 'Duncan', 'Parksville', 'Williams Lake', 'White Rock', 'Terrace', 'Cranbrook', 'Pitt Meadows', 'Fort Saint John', 'Port Alberni', 'Quesnel', 'Squamish', 'Powell River', 'Prince Rupert', 'Aldergrove'];
+            //Old Coord Table
+            var Coord = [
+                [49.248775, -122.980531], //any city
+                [51.644127, -121.295124], //100 mile
+                [49.050339, -122.304451], //abbotsford
+                [49.248775, -122.980531], //burnaby
+                [49.887836, -119.496592], //kelowna
+            ];
+
+            //City Table (sorted by pop.; unused)
+            //var CityByPop = ['Vancouver', 'Surrey', 'Burnaby', 'Richmond', 'Abbotsford', 'Kelowna', 'Port Coquitlam', 'Delta', 'Kamloops', 'Nanaimo', 'Victoria', 'Chilliwack', 'Maple Ridge', 'Prince George', 'New Westminster', 'North Vancouver', 'Penticton', 'West Vancouver', 'North Cowichan', 'Courtenay', 'Vernon', 'Central Okanagan', 'Campbell River', 'Port Moody', 'Walnut Grove', 'Langley', 'Duncan', 'Parksville', 'Williams Lake', 'White Rock', 'Terrace', 'Cranbrook', 'Pitt Meadows', 'Fort Saint John', 'Port Alberni', 'Quesnel', 'Squamish', 'Powell River', 'Prince Rupert', 'Aldergrove'];
 
             //City Table (sorted alphabetically)
-            var CityAlphabetic = [''];
+            var CityAlphabetic = ["Any City", "100 Mile House","Abbotsford","Aldergrove","Burnaby","Campbell River","Central Okanagan","Chilliwack","Courtenay","Cranbrook","Delta","Duncan","Fort Saint John","Kamloops","Kelowna","Langley","Maple Ridge","Nanaimo","New Westminster","North Cowichan","North Vancouver","Parksville","Penticton","Pitt Meadows","Port Alberni","Port Coquitlam","Port Moody","Powell River","Prince George","Prince Rupert","Quesnel","Richmond","Squamish","Surrey","Terrace","Vancouver","Vernon","Victoria","Walnut Grove","West Vancouver","White Rock","Williams Lake"];
 
             //Coord Table (Any City = Burnaby Default)
-            var Coord = [
-                [49.248775, -122.980531],
-                [51.644127, -121.295124],
-                [49.050339, -122.304451],
-                [49.248775, -122.980531],
-                [49.887836, -119.496592],
+            var CoordAplhabetic = [
+                [49.248775, -122.980531], //any city
+                [51.644127, -121.295124], //100 mile
+                [49.050339, -122.304451], //abbotsford
+                [0.0, 0.0],                 //aldergrove
+                [49.248775, -122.980531], //burnaby
+                [0.0, 0.0],                 //campble river
+                [0.0, 0.0],                 //central okanagan
+                [0.0, 0.0],                 //chilliwack
+                [0.0, 0.0],                 //courtenay
+                [0.0, 0.0],                 //cranbrook
+                [0.0, 0.0],                 //delta
+                [0.0, 0.0],                 //duncan
+                [0.0, 0.0],                 //fort s j
+                [0.0, 0.0],                 //
+                [49.887836, -119.496592], //kelowna
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0],                 //
+                [0.0, 0.0]                  //
+
             ];
+
 
             /*-------- Functions --------------*/
 
@@ -324,9 +414,8 @@
 
     </head>
 
-    <body>
 
-        <table>
+        <table class="tableForm">
             <tr>
                 <th colspan = "3"><h1>Step 1 - Select A Center</h1></th>
             </tr>
@@ -339,12 +428,53 @@
                     <!--//!@# need to fix optgroup potential error in laravel select form-->
                     <select id="city">
                         <!--default-->
-                        <option value="Any City" default>Any City</option>
-                        <!--cities-->
-                        <option value="100 Mile House">100 Mile House [1]</option>
-                        <option value="Abbotsford">Abbotsford [1]</option>
-                        <option value="Burnaby">Burnaby [2]</option>
-                        <option value="Kelowna">Kelowna [0]</option>
+                        <optgroup label="Default">
+                            <option value="Any City" default>Any City</option>
+                        </optgroup>
+                        <optgroup label="British Columbia">
+                            <!--cities, alphabetic-->
+                            <option value="100 Mile House">100 Mile House [1]</option>
+                            <option value="Abbotsford">Abbotsford [1]</option>
+                            <option value="Aldergrove">Aldergrove </option>
+                            <option value="Burnaby">Burnaby [2]</option>
+                            <option value="Campbell_River">Campbell River </option>
+                            <option value="Central_Okanagan">Central Okanagan </option>
+                            <option value="Chilliwack">Chilliwack </option>
+                            <option value="Courtenay">Courtenay </option>
+                            <option value="Cranbrook">Cranbrook </option>
+                            <option value="Delta">Delta </option>
+                            <option value="Duncan">Duncan </option>
+                            <option value="Fort_Saint_John">Fort Saint John </option>
+                            <option value="Kamloops">Kamloops </option>
+                            <option value="Kelowna">Kelowna[0]</option>
+                            <option value="Langley">Langley </option>
+                            <option value="Maple_Ridge">Maple Ridge </option>
+                            <option value="Nanaimo">Nanaimo </option>
+                            <option value="New_Westminster">New Westminster </option>
+                            <option value="North_Cowichan">North Cowichan </option>
+                            <option value="North_Vancouver">North Vancouver </option>
+                            <option value="Parksville">Parksville </option>
+                            <option value="Penticton">Penticton </option>
+                            <option value="Pitt_Meadows">Pitt Meadows </option>
+                            <option value="Port_Alberni">Port Alberni </option>
+                            <option value="Port_Coquitlam">Port Coquitlam </option>
+                            <option value="Port_Moody">Port Moody </option>
+                            <option value="Powell_River">Powell River </option>
+                            <option value="Prince_George">Prince George </option>
+                            <option value="Prince_Rupert">Prince Rupert </option>
+                            <option value="Quesnel">Quesnel </option>
+                            <option value="Richmond">Richmond </option>
+                            <option value="Squamish">Squamish </option>
+                            <option value="Surrey">Surrey </option>
+                            <option value="Terrace">Terrace </option>
+                            <option value="Vancouver">Vancouver </option>
+                            <option value="Vernon">Vernon </option>
+                            <option value="Victoria">Victoria </option>
+                            <option value="Walnut_Grove">Walnut Grove </option>
+                            <option value="West_Vancouver">West_Vancouver </option>
+                            <option value="White_Rock">White Rock</option>
+                            <option value="Williams_Lake">Williams Lake </option>
+                        </optgroup>
                     </select>
 
                 </td>
@@ -366,12 +496,16 @@
             </tr>
         </table>
 
+
+
         <!--Map Element-->
         <div id="map-canvas"></div>
         <br>
 
+
+        <div class="response">
         <!--New Form Table-->
-        <table>
+        <table class="tableForm">
             <tr>
                 <th colspan = "2"><h1>Step 2 - Complete Your Exam Form</h1></th>
             </tr>
@@ -382,19 +516,19 @@
                 <th colspan = "2"><h2><em>Invigilation Center</em></h2></th>
             </tr>
             <tr>
-                <td> {!! Form::label('centerName','Center Name:') //Center Name!!} </td>
+                <td class="required"> {!! Form::label('centerName','Center Name:') //Center Name!!} </td>
                 <td> {!! Form::text('centerName') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('centerStreetAddress','Center Street Address:') //Center Street Address!!} </td>
+                <td class="required"> {!! Form::label('centerStreetAddress','Center Street Address:') //Center Street Address!!} </td>
                 <td> {!! Form::text('centerStreetAddress') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('centerCity','Center City:') //Center City!!} </td>
+                <td class="required"> {!! Form::label('centerCity','Center City:') //Center City!!} </td>
                 <td> {!! Form::text('centerCity') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('centerProvince','Center Province:') //Center Province!!} </td>
+                <td class="required"> {!! Form::label('centerProvince','Center Province:') //Center Province!!} </td>
                 <td>
                     {!! Form::select('centerProvince',[
                         'Canada' => [
@@ -412,7 +546,6 @@
                             'Northwest_Territories' => 'Northwest Territories',
                             'Nunavut' => 'Nunavut',
                         ],
-                        'British_Columbia'
                     ]); !!}
                 </td>
             </tr>
@@ -429,28 +562,28 @@
                 <th colspan = "2"><h2><em>Examinee (i.e. You)</em></h2></th>
             </tr>
             <tr>
-                <td> {!! Form::label('studentFirstName','Student First Name:') //Student First Name!!} </td>
+                <td class="required"> {!! Form::label('studentFirstName','Student First Name:') //Student First Name!!} </td>
                 <td> {!! Form::text('studentFirstName') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('studentLastName','Student Last Name:') //Student Last Name!!} </td>
+                <td class="required"> {!! Form::label('studentLastName','Student Last Name:') //Student Last Name!!} </td>
                 <td> {!! Form::text('studentLastName') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('studentPhone','Student Phone Number:') //Student Phone Number!!} </td>
+                <td class="required"> {!! Form::label('studentPhone','Student Phone Number:') //Student Phone Number!!} </td>
                 <td> {!! Form::number('studentPhone') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('studentEmail','Student Email Address:') //Student Email!!} </td>
+                <td class="required"> {!! Form::label('studentEmail','Student Email Address:') //Student Email!!} </td>
                 <td> {!! Form::email('studentEmail') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('examDate1','Exam Date (First Choice):') //Exam Date 1!!} </td>
+                <td class="required"> {!! Form::label('examDate1','Exam Date (First Choice):') //Exam Date 1!!} </td>
                 <td> {!! Form::date('examDate1', \Carbon\Carbon::now()); !!} </td>
 
             </tr>
             <tr>
-                <td> {!! Form::label('examDate2','Exam Date (Second Choice):') //Exam Date 2!!} </td>
+                <td class="required"> {!! Form::label('examDate2','Exam Date (Second Choice):') //Exam Date 2!!} </td>
                 <td> {!! Form::date('examDate2', \Carbon\Carbon::now()); !!} </td>
             </tr>
             <tr>
@@ -467,19 +600,19 @@
                 <th colspan = "2"><h2><em>Institution</em></h2></th>
             </tr>
             <tr>
-                <td> {!! Form::label('institutionName','Institution Name:') //Institution Name!!} </td>
+                <td class="required"> {!! Form::label('institutionName','Institution Name:') //Institution Name!!} </td>
                 <td> {!! Form::text('institutionName') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('institutionStreetAddress','Institution Street Address:') //Institution Street Address!!} </td>
+                <td class="required"> {!! Form::label('institutionStreetAddress','Institution Street Address:') //Institution Street Address!!} </td>
                 <td> {!! Form::text('institutionStreetAddress') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('institutionCity','Institution City:') //Institution City!!} </td>
+                <td class="required"> {!! Form::label('institutionCity','Institution City:') //Institution City!!} </td>
                 <td> {!! Form::text('institutionCity') !!} </td>
             </tr>
             <tr>
-                <td> {!! Form::label('institutionProvince','Institution Province:') //Institution Province!!} </td>
+                <td class="required"> {!! Form::label('institutionProvince','Institution Province:') //Institution Province!!} </td>
                 <td> {!! Form::select('institutionProvince',[
                     'Canada' => [
                         'British_Columbia' => 'British Columbia' ,
@@ -496,7 +629,6 @@
                         'Northwest_Territories' => 'Northwest Territories',
                         'Nunavut' => 'Nunavut',
                     ],
-                    'British_Columbia'
                 ]); !!} </td>
             </tr>
             <tr>
@@ -525,20 +657,20 @@
                 <th colspan = "2"><h2><em>Exam</em></h2></th>
             </tr>
             <tr>
-                <td> {!! Form::label('courseNumber','Course Number:') //Course Number!!} </td>
+                <td class="required"> {!! Form::label('courseNumber','Course Number:') //Course Number!!} </td>
                 <td> {!! Form::text('courseNumber') !!} </td>
             </tr>
             <tr>
-                <td>{!! Form::label('courseMidtermOrFinal','Midterm or Final:') //Course Midterm or Final!!}</td>
-                <td>{!! Form::select('courseMidtermOrFinal', ['Midterm' => 'Midterm', 'Final' => 'Final'], 'Final'); !!}</td>
+                <td class="required"> {!! Form::label('courseMidtermOrFinal','Midterm or Final:') //Course Midterm or Final!!}</td>
+                <td> {!! Form::select('courseMidtermOrFinal', ['Midterm' => 'Midterm', 'Final' => 'Final'], 'Final'); !!}</td>
             </tr>
             <tr>
-                <td>{!! Form::label('courseExamType','Exam Type:') //Course Exam Type!!}</td>
-                <td>{!! Form::select('courseExamType', ['Paper' => 'Paper', 'Online' => 'Online', 'Other' => 'Other'], 'Paper'); !!}</td>
+                <td class="required"> {!! Form::label('courseExamType','Exam Type:') //Course Exam Type!!}</td>
+                <td> {!! Form::select('courseExamType', ['Paper' => 'Paper', 'Online' => 'Online', 'Other' => 'Other'], 'Paper'); !!}</td>
             </tr>
             <tr>
-                <td>{!! Form::label('courseCompReq','Computer Required:') //Course Computer Required!!}</td>
-                <td>{!! Form::select('courseCompReq', ['Yes'=>'Yes','No'=>'No'], 'No'); !!}</td>
+                <td class="required"> {!! Form::label('courseCompReq','Computer Required:') //Course Computer Required!!}</td>
+                <td> {!! Form::select('courseCompReq', ['Yes'=>'Yes','No'=>'No'], 'No'); !!}</td>
             </tr>
             <tr>
                 <td> {!! Form::label('courseAddReqs','Additional Requirements or Information:') //Course Additional Requirements!!} </td>
@@ -560,6 +692,14 @@
                 <td>{!! Form::submit('Book Exam!'); !!}</td>
             </tr>
         </table>
+        </div>
+
+
+
+
+
+
+
 
     </body>
 </html>
